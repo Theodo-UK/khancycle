a2be610d2057c8e0a4b8f66e9fe10a1846f4c8de
Hari Sriskantha <haris@theodo.co.uk>
2016-06-01 17:32:38 +0100

    [14] Fixed remaining test.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import StationList from '../StationList/StationList';
 
+const stationListElement = function () {
+  return <StationList />;
+};
+
 class Home extends Component {
   render() {
-    return (
-      <StationList />
-    );
+    return stationListElement();
   }
 }
 
