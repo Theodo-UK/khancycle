56a038aa1234eea17093fcd12bfe035ab51d69a5
Farhan Mannan <farhanmannan@mac.com>
2016-06-01 12:52:01 +0100

    [40] update index.android and fix circle/install_nvm.js permissions

--- index.android.js
+++ index.android.js
@@ -13,19 +13,7 @@ import {
 
 class KhanCycle extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return (<Home />)
   }
 }
 
