9374ecd9a457ac75f4fb4783220798911010833a
Rich <piemonkey@gmail.com>
2016-06-03 16:09:40 +0100

    Added non-functional report broken bikes button

--- src/components/StationDetail/StationDetail.js
+++ src/components/StationDetail/StationDetail.js
@@ -1,8 +1,31 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { Text, View, TouchableHighlight } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import styles from './StationDetail.style';
 
 class StationDetail extends Component {
+  componentWillMount() {
+    Actions.refresh({ title: this.props.rowData.extra.name });
+  }
+
+  reportBrokenBikes() {
+    console.warn('reported!');
+  }
+
   render() {
-    return null;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.detail}>Bikes: {this.props.rowData.free_bikes}</Text>
+        <Text style={styles.detail}>Spaces: {this.props.rowData.empty_slots}</Text>
+        <TouchableHighlight onPress={this.reportBrokenBikes}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>
+              Report Broken Bikes
+            </Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
   }
 }
 
--- src/components/StationDetail/StationDetail.style.js
+++ src/components/StationDetail/StationDetail.style.js
@@ -1,9 +1,28 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  text: {
-    color: '#cccccc',
+  container: {
+    margin: 20,
+    marginTop: 80,
   },
+  detail: {
+    color: '#7f7f7f',
+    marginRight: 30,
+    fontSize: 12,
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#F6F6F6',
+    borderColor: '#7f7f7f',
+    borderWidth: 1,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: '#4f4c4c',
+  }
 });
 
 export default styles;
