399f0b0f73570357d2794d9bc3119147d7a2661e
Rich <piemonkey@gmail.com>
2016-06-06 15:34:45 +0100

    Refactor StationList container

--- src/containers/StationList/StationList.js
+++ src/containers/StationList/StationList.js
@@ -1,16 +1,16 @@
 import { connect } from 'react-redux';
 import StationList from '../../components/StationList/StationList';
-import * as stationsActions from '../../actions/stations';
-import * as locationActions from '../../actions/location';
+import { updateStations } from '../../actions/stations';
+import { updateLocation } from '../../actions/location';
 
 const mapStateToProps = (state) => ({
   stations: state.stations,
   location: state.location,
 });
-const mapDispatchToProps = (dispatch) => ({
-  updateStations: (stations) => dispatch(stationsActions.updateStations(stations)),
-  updateLocation: (latitude, longitude) => dispatch(locationActions.updateLocation(latitude, longitude)),
-});
+const mapDispatchToProps = {
+  updateStations,
+  updateLocation,
+};
 
 const StationListContainer = connect(
   mapStateToProps,
