4dd781eacddd549a21daa1f16de011bd9c9c293c
Hari Sriskantha <haris@theodo.co.uk>
2016-06-07 11:36:17 +0100

    [54] Use react-native-maps on iOS.

--- src/components/StationListMap/StationListMap.style.js
+++ src/components/StationListMap/StationListMap.style.js
@@ -1,8 +1,9 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
   map: {
     flex: 1,
+    width: Dimensions.get('window').width,
   },
   container: {
     flex: 1,
