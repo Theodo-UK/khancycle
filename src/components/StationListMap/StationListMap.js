1a285183ce2b47de11a4e0a5b059205e94047234
Hari Sriskantha <haris@theodo.co.uk>
2016-06-07 12:05:37 +0100

    [54] Added current location to map.

--- src/components/StationListMap/StationListMap.js
+++ src/components/StationListMap/StationListMap.js
@@ -3,23 +3,52 @@ import { View } from 'react-native';
 import styles from './StationListMap.style';
 import MapView from 'react-native-maps';
 
-const jsx = () =>
-  <View style={styles.container}>
-    <MapView
-      style={styles.map}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-  </View>;
+const jsx = (initialRegion) => {
+  let jsxView = null;
+  if (initialRegion != null) {
+    const coordinate = { latitude: initialRegion.latitude, longitude: initialRegion.longitude };
+    jsxView = (
+      <View style={styles.container}>
+        <MapView style={styles.map} initialRegion={initialRegion}>
+          <MapView.Marker
+            coordinate={coordinate}
+            title={"Current Location"}
+            pinColor={"#0000ff"}
+          />
+        </MapView>
+      </View>
+    );
+  } else {
+    jsxView = (
+      <View style={styles.container}>
+        <MapView style={styles.map} initialRegion={initialRegion} />
+      </View>
+    );
+  }
+  return jsxView;
+};
 
 class StationListMap extends Component {
   render() {
-    return jsx();
+    const location = this.props.location;
+    let initialRegion = null;
+    if (location.latitude != null && location.longitude != null) {
+      initialRegion = {
+        latitude: location.latitude,
+        longitude: location.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      };
+    }
+    return jsx(initialRegion);
   }
 }
+
+StationListMap.propTypes = {
+  location: React.PropTypes.shape({
+    latitude: React.PropTypes.number,
+    longitude: React.PropTypes.number,
+  }),
+};
 
 export default StationListMap;
--- src/components/StationListMap/StationListMap.style.js
+++ src/components/StationListMap/StationListMap.style.js
@@ -3,10 +3,12 @@ import { StyleSheet, Dimensions } from 'react-native';
 const styles = StyleSheet.create({
   map: {
     flex: 1,
-    width: Dimensions.get('window').width,
   },
   container: {
     flex: 1,
+    width: Dimensions.get('window').width,
+    borderBottomWidth: 1,
+    borderBottomColor: '#999999',
   },
 });
 
