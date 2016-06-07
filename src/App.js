660970633e6e468b810476cef0e047ea843a7388
Rich <piemonkey@gmail.com>
2016-06-07 11:37:47 +0100

    Add firebase SDK and save number of bikes broken on report

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Component } from 'react';
 import rootReducer from './reducers/index';
 import routes from './config/routes';
+import * as firebase from './services/firebase';
 
 // Create the default state and store
 const defaultState = { stations: [], location: {} };
-const store = createStore(rootReducer, defaultState);
+const store = applyMiddleware(firebase.middleware)(createStore)(rootReducer, defaultState);
 
 class App extends Component {
   render() {
---
+++ src/services/firebase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const config = {
+  apiKey: 'AIzaSyBzoiGUxpA1S4of9WTfVvYCgBSWsFFQnc8',
+  authDomain: 'project-908306213235132709.firebaseapp.com',
+  databaseURL: 'https://project-908306213235132709.firebaseio.com',
+  storageBucket: 'project-908306213235132709.appspot.com',
+};
+const database = firebase.initializeApp(config).database();
+const bikeReportsRef = database.ref('brokenBikes');
+
+// const brokenBikesRef = Firebase.database().ref('brokenBikes');
+const reportBrokenBikes = (stationId, number) => {
+  bikeReportsRef.child(stationId).set(number)
+    .then(() => console.warn('success'))
+    .catch(err => console.warn(`error: ${err}`));
+};
+
+export const middleware = () => next => action => {
+  if (action.type === 'REPORT_BROKEN') {
+    reportBrokenBikes(action.stationId, action.number);
+  }
+  return next(action);
+};
