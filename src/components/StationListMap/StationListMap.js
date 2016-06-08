16d81c0ade855db0ae53218d5ccd049651178286
Farhan Mannan <farhanmannan@mac.com>
2016-06-08 14:32:54 +0100

    [46] remove container View in StationListMap and center on Charing Cross

--- src/components/StationListMap/StationListMap.js
+++ src/components/StationListMap/StationListMap.js
@@ -4,12 +4,12 @@ import styles from './StationListMap.style';
 import Mapbox from 'react-native-mapbox-gl';
 
 const jsx = () =>
-  <View style={styles.container}>
     <Mapbox
       style={styles.map}
       accessToken='pk.eyJ1IjoidGhlb2RvdWsiLCJhIjoiY2lwNm55ZXdlMDAwaHVkbm05cDNtNzA3cCJ9.hhMg_ZQceBIAnEvd5tu6Hw'
-    />
-  </View>;
+      centerCoordinate={ { latitude: 51.5073, longitude: -0.1276 } }
+      zoomLevel={14}
+    />;
 
 class StationListMap extends Component {
   render() {
--- src/components/StationListMap/StationListMap.style.js
+++ src/components/StationListMap/StationListMap.style.js
@@ -1,9 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   map: {
     flex: 1,
   }
