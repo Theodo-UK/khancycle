e59dd764b9a5a697ee1a0c835e51b8454bccc75b
Farhan Mannan <farhanmannan@mac.com>
2016-06-08 11:25:02 +0100

    [46] add MapBox map above StationList on iOS

--- src/components/StationListMap/StationListMap.style.js
+++ src/components/StationListMap/StationListMap.style.js
@@ -3,8 +3,10 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 100,
   },
+  map: {
+    flex: 1,
+  }
 });
 
 export default styles;
