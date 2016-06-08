c5d5a23631507d64328f65bb6270f53f015189bb
Farhan Mannan <farhanmannan@mac.com>
2016-06-08 14:34:29 +0100

    [46] lint

--- src/components/StationListMap/StationListMap.js
+++ src/components/StationListMap/StationListMap.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
 import styles from './StationListMap.style';
 import Mapbox from 'react-native-mapbox-gl';
 
 const jsx = () =>
-    <Mapbox
-      style={styles.map}
-      accessToken='pk.eyJ1IjoidGhlb2RvdWsiLCJhIjoiY2lwNm55ZXdlMDAwaHVkbm05cDNtNzA3cCJ9.hhMg_ZQceBIAnEvd5tu6Hw'
-      centerCoordinate={ { latitude: 51.5073, longitude: -0.1276 } }
-      zoomLevel={14}
-    />;
+  <Mapbox
+    style={styles.map}
+    accessToken="pk.eyJ1IjoidGhlb2RvdWsiLCJhIjoiY2lwNm55ZXdlMDAwaHVkbm05cDNtNzA3cCJ9.hhMg_ZQceBIAnEvd5tu6Hw"
+    centerCoordinate={{ latitude: 51.5073, longitude: -0.1276 }}
+    zoomLevel={14}
+  />;
 
 class StationListMap extends Component {
   render() {
