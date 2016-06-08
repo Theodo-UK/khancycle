8a58c6958256410fcea6440b72b5d9a486940e99
Hari Sriskantha <haris@theodo.co.uk>
2016-06-08 17:05:09 +0100

    [68] Post-review: restored 'container' variable names.

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Scene, Router, Actions } from 'react-native-router-flux';
-import * as Stations from '../containers/Stations';
-import * as StationDetail from '../containers/StationDetail';
-import * as About from '../containers/About';
+import * as StationsContainer from '../containers/Stations';
+import * as StationDetailContainer from '../containers/StationDetail';
+import * as AboutContainer from '../containers/About';
 
 
 export default (store) => (
@@ -12,14 +12,14 @@ export default (store) => (
       <Scene key="root">
         <Scene
           key={'stations'}
-          component={Stations.Stations}
+          component={StationsContainer.StationsContainer}
           title={'KhanCycle'}
           rightTitle={'?'}
           onRight={() => Actions.about()}
           initial
         />
-        <Scene key={'stationDetail'} component={StationDetail.StationDetail} title={'Station Detail'} />
-        <Scene key={'about'} component={About.About} title={'About'} />
+        <Scene key={'stationDetail'} component={StationDetailContainer.StationDetailContainer} title={'Station Detail'} />
+        <Scene key={'about'} component={AboutContainer.AboutContainer} title={'About'} />
       </Scene>
     </Router>
   </Provider>
