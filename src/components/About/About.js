52f538e40e7786c681880f0db32138d1948bb282
Hari Sriskantha <haris@theodo.co.uk>
2016-06-08 10:28:23 +0100

    [68] Added blank about page.

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class About extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+export default About;
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import * as StationsContainer from '../containers/Stations';
 import * as StationDetail from '../containers/StationDetail';
+import * as About from '../containers/About';
 
 
 export default (store) => (
   <Provider store={store}>
     <Router>
       <Scene key="root">
-        <Scene key={'stations'} component={StationsContainer.StationsContainer} title={'KhanCycle'} initial />
+        <Scene key={'stations'} component={StationsContainer.StationsContainer} title={'KhanCycle'}
+          rightTitle={'About'} onRight={() => Actions.about()} initial />
         <Scene key={'stationDetail'} component={StationDetail.StationDetail} title={'Station Detail'} />
+        <Scene key={'about'} component={About.About} title={'About'} />
       </Scene>
     </Router>
   </Provider>
