d1139965c4179e981bf8a41f5ae5769e46faf96c
Farhan Mannan <farhanmannan@mac.com>
2016-06-08 17:58:30 +0100

    [65] display nearest stations on map

--- src/components/StationListMap/StationListMap.js
+++ src/components/StationListMap/StationListMap.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { List } from 'immutable';
 
 import styles from './StationListMap.style';
 import Mapbox from 'react-native-mapbox-gl';
 
-const jsx = () =>
-  <Mapbox
-    style={styles.map}
-    accessToken="pk.eyJ1IjoidGhlb2RvdWsiLCJhIjoiY2lwNm55ZXdlMDAwaHVkbm05cDNtNzA3cCJ9.hhMg_ZQceBIAnEvd5tu6Hw"
-    centerCoordinate={{ latitude: 51.5073, longitude: -0.1276 }}
-    zoomLevel={14}
-  />;
+const jsx = pros =>
+  <View style={styles.container}>
+    <Mapbox
+      style={styles.map}
+      accessToken="pk.eyJ1IjoidGhlb2RvdWsiLCJhIjoiY2lwNm55ZXdlMDAwaHVkbm05cDNtNzA3cCJ9.hhMg_ZQceBIAnEvd5tu6Hw"
+      centerCoordinate={{ latitude: 51.5073, longitude: -0.1276 }}
+      zoomLevel={14}
+    />
+    <Text>{pros.nearestStations.map(s => s.name).join('\n')}</Text>
+  </View>;
 
 class StationListMap extends Component {
   render() {
-    return jsx();
+    return jsx(this.props);
   }
 }
 
