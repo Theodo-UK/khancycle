50429616dfe47bae705d187043c5305cd9f9f4e2
Farhan Mannan <farhanmannan@mac.com>
2016-06-09 16:36:07 +0100

    [54] add automatic zoom to include 5 nearest stations and current location

--- src/components/StationListMap/StationListMap.js
+++ src/components/StationListMap/StationListMap.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { List } from 'immutable';
+import Mapbox from 'react-native-mapbox-gl';
+import reactMixin from 'react-mixin';
 
 import styles from './StationListMap.style';
-import Mapbox from 'react-native-mapbox-gl';
 
 const mapRef = 'mapRef';
 // Note that Mapbox needs a centerCoordinate even if userTrackingMode is
@@ -16,35 +16,55 @@ const jsx = (annotations, location) =>
     centerCoordinate={location}
     userTrackingMode={1}
     showsUserLocation
-    zoomLevel={14}
     annotations={annotations}
   />;
 
 class StationListMap extends Component {
-  render() {
-    const propsLocation = this.props.location;
-    const location = {
-      latitude: (propsLocation.latitude != null) ? propsLocation.latitude : 0,
-      longitude: (propsLocation.longitude != null) ? propsLocation.longitude : 0,
-    };
-    let annotations = this.props.nearestStations
-      .map(st =>this.stationToAnnotation(st))
-      .toArray();
-    return jsx(annotations, location);
+  componentDidUpdate() {
+    const stations = this.props.nearestStations;
+    const location = this.props.location;
+    if (stations.size) {
+      const latitudes = stations.map(s => s.latitude).toArray();
+      const longitudes = stations.map(s => s.longitude).toArray();
+      // include the current location in the bounding box
+      if (location.latitude) { latitudes.push(location.latitude); }
+      if (location.longitude) { longitudes.push(location.longitude); }
+      // setVisibleCoordinateBoundsAnimated comes from Mapbox.Mixin
+      this.setVisibleCoordinateBoundsAnimated(
+        mapRef,
+        // SW corner of bounding box
+        Math.min(...latitudes), Math.min(...longitudes),
+        // NE corner of bounding box
+        Math.max(...latitudes), Math.max(...longitudes),
+        // padding - top needs more so it's not under header
+        100, 50, 50, 50
+      );
+    }
   }
   stationToAnnotation(station) {
-    let [stationId, stationName] = station.name.split(' - ');
+    const [stationId, stationName] = station.name.split(' - ');
     return {
       id: stationId,
       title: stationName,
       coordinates: [station.latitude, station.longitude],
       subtitle: `Bikes: ${station.free_bikes}, spaces: ${station.empty_slots}`,
       type: 'point',
-    }
+    };
+  }
+  render() {
+    const propsLocation = this.props.location;
+    const location = {
+      latitude: (propsLocation.latitude != null) ? propsLocation.latitude : 0,
+      longitude: (propsLocation.longitude != null) ? propsLocation.longitude : 0,
+    };
+    const annotations = this.props.nearestStations
+    .map(st => this.stationToAnnotation(st))
+    .toArray();
+    return jsx(annotations, location);
   }
 }
 
-StationListMap.mixins = [Mapbox.Mixin];
+reactMixin(StationListMap.prototype, Mapbox.Mixin);
 
 StationListMap.propTypes = {
   nearestStations: React.PropTypes.instanceOf(List),
