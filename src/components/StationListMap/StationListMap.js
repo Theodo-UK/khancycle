ddab9905ddbcfcad324617eb1fabadfc64e0cdc4
Farhan Mannan <farhanmannan@mac.com>
2016-06-09 10:58:05 +0100

    [54] add station markers with no automatic zoom

--- src/components/StationListMap/StationListMap.js
+++ src/components/StationListMap/StationListMap.js
@@ -5,20 +5,20 @@ import { List } from 'immutable';
 import styles from './StationListMap.style';
 import Mapbox from 'react-native-mapbox-gl';
 
+const mapRef = 'mapRef';
 // Note that Mapbox needs a centerCoordinate even if userTrackingMode is
 // enabled -- otherwise, Mapbox will ignore the zoom level.
-const jsx = (pros, location) =>
-  <View style={styles.container}>
-    <Mapbox
-      style={styles.map}
-      accessToken="pk.eyJ1IjoidGhlb2RvdWsiLCJhIjoiY2lwNm55ZXdlMDAwaHVkbm05cDNtNzA3cCJ9.hhMg_ZQceBIAnEvd5tu6Hw"
-      centerCoordinate={location}
-      userTrackingMode={1}
-      showsUserLocation
-      zoomLevel={14}
-    />
-    <Text>{pros.nearestStations.map(s => s.name).join('\n')}</Text>
-  </View>;
+const jsx = (annotations, location) =>
+  <Mapbox
+    ref={mapRef}
+    style={styles.map}
+    accessToken="pk.eyJ1IjoidGhlb2RvdWsiLCJhIjoiY2lwNm55ZXdlMDAwaHVkbm05cDNtNzA3cCJ9.hhMg_ZQceBIAnEvd5tu6Hw"
+    centerCoordinate={location}
+    userTrackingMode={1}
+    showsUserLocation
+    zoomLevel={14}
+    annotations={annotations}
+  />;
 
 class StationListMap extends Component {
   render() {
@@ -27,7 +27,20 @@ class StationListMap extends Component {
       latitude: (propsLocation.latitude != null) ? propsLocation.latitude : 0,
       longitude: (propsLocation.longitude != null) ? propsLocation.longitude : 0,
     };
-    return jsx(this.props, location);
+    let annotations = this.props.nearestStations
+      .map(st =>this.stationToAnnotation(st))
+      .toArray();
+    return jsx(annotations, location);
+  }
+  stationToAnnotation(station) {
+    let [stationId, stationName] = station.name.split(' - ');
+    return {
+      id: stationId,
+      title: stationName,
+      coordinates: [station.latitude, station.longitude],
+      subtitle: `Bikes: ${station.free_bikes}, spaces: ${station.empty_slots}`,
+      type: 'point',
+    }
   }
 }
 
