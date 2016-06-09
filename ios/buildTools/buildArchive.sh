19294feab15143110b5d0a5d2503cedc158ce4de
Hari Sriskantha <haris@theodo.co.uk>
2016-06-09 15:14:28 +0100

    [73] Created build script for iOS.

---
+++ ios/buildTools/buildArchive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Note: run this from the 'ios' folder -- not here in the 'ios/buildTools' folder!
+
+# Clean project
+xcodebuild clean -project KhanCycle.xcodeproj -configuration Release -alltargets
+
+# Copy release version of AppDelegate.m
+cp BuildTools/AppDelegate-Release.m KhanCycle/AppDelegate.m
+
+# Create archive
+xcodebuild archive -project KhanCycle.xcodeproj -scheme KhanCycle -archivePath KhanCycle.xcarchive
+
+# Save archive for ad-hoc deployment (You'll get a deprecation warning, but the
+# new method doesn't work correctly -- http://stackoverflow.com/a/34003826).
+xcodebuild -exportArchive -archivePath KhanCycle.xcarchive -exportPath KhanCycle -exportFormat ipa -exportProvisioningProfile "KhanCycle: Ad-Hoc Distribution"
