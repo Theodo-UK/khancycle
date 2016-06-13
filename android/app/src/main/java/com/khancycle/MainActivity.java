fce2ab758ef537e0e67f71e7b2f144aaf4b80ba2
Farhan Mannan <farhanmannan@mac.com>
2016-06-13 11:43:37 +0100

    [60] install react-native-package-info on Android and iOS

--- android/app/src/main/java/com/khancycle/MainActivity.java
+++ android/app/src/main/java/com/khancycle/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,7 +36,8 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            new ReactNativeMapboxGLPackage()
+            new ReactNativeMapboxGLPackage(),
+            new RNDeviceInfo()
         );
     }
 }
